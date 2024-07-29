4c3ff72fd49c86f95d0f351524563d2c45684d93
hir01 <glazunovden@yandex.ru>
2024-07-29 15:51:26 +0300

    fix

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -25,22 +25,28 @@ export const Contact = () => {
                 <a href="mailto:zakup@loaderpro.ru">zakup@loaderpro.ru</a>
               </div>
               <div class="contact__info-social">
-                <a href="#">
+                <a href="https://wa.me/79643421256" target="_blank" rel="noreferrer">
                   <picture>
                     <source srcSet="/assets/images/contact/wh.png" />
                     <img src="/assets/images/contact/wh.png" alt="" decoding="async" />
                   </picture>
                 </a>
-                <a href="#">
+                <a href="https://t.me/loaderpro" target="_blank" rel="noreferrer">
                   <picture>
                     <source srcSet="/assets/images/contact/tg.png" />
                     <img src="/assets/images/contact/tg.png" alt="" decoding="async" />
                   </picture>
                 </a>
-                <a href="#">
+                <a href="https://vk.com/loaderproru" target="_blank" rel="noreferrer">
                   <picture>
                     <source srcSet="/assets/images/contact/vk.png" />
                     <img src="/assets/images/contact/vk.png" alt="" decoding="async" />
+                  </picture>
+                </a>
+                <a href="https://www.youtube.com/@loaderpro" target="_blank" rel="noreferrer">
+                  <picture>
+                    <source srcSet="/assets/images/contact/youtube.png" />
+                    <img src="/assets/images/contact/youtube.png" alt="" decoding="async" />
                   </picture>
                 </a>
               </div>
